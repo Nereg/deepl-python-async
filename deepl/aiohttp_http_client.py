24fd1328b0aadfbb318eed6162537a3b92c0898d
Nereg <47698366+Nereg@users.noreply.github.com>
2024-06-30 06:29:31 +0200

    Implemented SSL bypasses

--- deepl/aiohttp_http_client.py
+++ deepl/aiohttp_http_client.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by an MIT
 # license that can be found in the LICENSE file.
 import aiohttp
+import ssl
 from typing import Union, Dict
 
 from .translator_base import HttpResponse
@@ -16,8 +17,23 @@ class AioHttpHttpClient(IAsyncHttpClient):
         proxy: Union[Dict, str, None] = None,
         verify_ssl: Union[bool, str, None] = None,
     ):
-        self._session = aiohttp.ClientSession()
-        # TODO proxy, verify_ssl
+        self._ssl = verify_ssl
+        # if we were passed a path to a file/folder with CA certificates
+        if type(self._ssl) is str:
+            # pass them onto SSL context
+            self._session = aiohttp.ClientSession(
+                connector=aiohttp.TCPConnector(
+                    ssl_context=ssl.create_default_context(
+                        ssl.Purpose.SERVER_AUTH,
+                        cafile=self._ssl,
+                        capath=self._ssl,
+                    )
+                )
+            )
+        else:
+            # create default client session
+            self._session = aiohttp.ClientSession()
+        # TODO proxy
 
         super().__init__()
 
@@ -38,6 +54,8 @@ class AioHttpHttpClient(IAsyncHttpClient):
             headers=request.headers,
             data=request.data,
             json=request.json,
+            # disable the SSL verification, if we don't have a path to certificates
+            ssl=self._ssl if type(self._ssl) is bool else None,
             # stream=request.stream, TODO
         ) as response:
             content = await response.text()
